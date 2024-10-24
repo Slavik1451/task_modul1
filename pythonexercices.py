9311f828b742f3e21c6e5656bc0126350cf80597
Slavik1451 <slavik.rudkovskii@gmail.com>
2024-10-24 04:17:04 +0300

    Add files via upload

---
+++ pythonexercices.py
@@ -0,0 +1,5 @@
+homework_count = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_homework = hours_spent / homework_count
+print(f'Курс: {course_name}, всего задач: {homework_count}, затрачено часов: {hours_spent}, среднее время выполнения задач: {time_per_homework} часа.')
