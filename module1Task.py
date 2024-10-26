94a2ad21fc5480410756d8df6435cf992fabe03b
Slavik1451 <slavik.rudkovskii@gmail.com>
2024-10-27 00:33:56 +0300

    module1Task.py

--- module1Task.py
+++ module1Task.py
@@ -1,13 +1,7 @@
-from time import sleep
-empty_list = []
-while True:
-    user_input = int(input('Число: '))
-    if user_input != 0:
-        empty_list.append(user_input)
-        sleep(4)
-    else:
-        break
-print(f'Введенные числа: {empty_list}')
-sum_list = sum(empty_list)
-print(f'Сумма чисел: {sum_list}')
-
+rades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+list_students = list(students)
+students_grades = {}
+for i in range(len(rades)):
+    students_grades[list_students[i]] = rades[i]
+print(students_grades)
