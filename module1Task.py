87a1e44a4ac30378f21c1f50c579632f95e63cd7
Slavik1451 <slavik.rudkovskii@gmail.com>
2024-10-28 18:10:52 +0300

    module1Task.py

--- module1Task.py
+++ module1Task.py
@@ -1,7 +1,8 @@
-rades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 list_students = list(students)
 students_grades = {}
-for i in range(len(rades)):
-    students_grades[list_students[i]] = rades[i]
+for i in range(len(grades)):
+    grades_ = grades[i]
+    students_grades[list_students[i]] = sum(grades_) / len(grades_)
 print(students_grades)
