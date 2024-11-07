d86a88e8e9f39ce8a5a04025de1d497b48feb24c
Slavik1451 <slavik.rudkovskii@gmail.com>
2024-11-07 06:38:31 +0300

    Update module_2_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,20 +1,9 @@
-def send_email(message, recipient, sender="university.help@gmail.com"):
-    tuple_ = ('.com', '.ru', '.net')
-    if ('@' not in recipient or '@' not in sender
-            or not recipient.endswith(tuple_)
-            or not sender.endswith(tuple_)):
-
-        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
-
-    elif sender == recipient:
-        print('Нельзя отправить письмо самому себе!')
-    elif sender == 'university.help@gmail.com':
-        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
-    else:
-        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
-
-
-send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
-send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
-send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
-send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+first = int(input('Введите первое число: '))
+second = int(input('Введите второе число: '))
+third = int(input('Введите третье число: '))
+if first == second and second == third:
+    print('3')
+elif first == second or second == third or first == third:
+    print('2')
+else:
+    print('0')
