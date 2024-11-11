d23d1975b22926052a1a2c73a397dbfdaeb74538
Slavik1451 <slavik.rudkovskii@gmail.com>
2024-11-11 22:18:36 +0300

    Update pythonexercices1.py

--- pythonexercices1.py
+++ pythonexercices1.py
@@ -1,13 +1,17 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes, not_primes = [], []
-for num in range(1, len(numbers)):
+
+for num in numbers:
+    if num == 1:
+        continue
     is_prime = True
-    for j in range(2, numbers[num]):
-        if numbers[num] % j == 0:
+    for j in range(2, num):
+        if num % j == 0:
             is_prime = False
             break
     if is_prime:
-        primes.append(numbers[num])
+        primes.append(num)
     else:
-        not_primes.append(numbers[num])
+        not_primes.append(num)
+
 print(f'Primes:{primes}\nNot_primes:{not_primes}')
