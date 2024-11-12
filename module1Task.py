be5fe02ac74447d153c975efd8f336c2cd3dbb2f
Slavik1451 <slavik.rudkovskii@gmail.com>
2024-11-12 08:44:53 +0300

    Update module1Task.py

--- module1Task.py
+++ module1Task.py
@@ -1,8 +1,9 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
-students = ('Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron')
-list_students = list(students)
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+list_students = list(sorted((students)))
 students_grades = {}
 for i in range(len(grades)):
     grades_ = grades[i]
     students_grades[list_students[i]] = sum(grades_) / len(grades_)
 print(students_grades)
+
